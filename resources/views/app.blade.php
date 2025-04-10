4e783b888e42ea5dee262fb3432905347e2e647d
zart227 <zart227@gmail.com>
2025-04-10 12:42:16 +0300

    fix(a11y): add lang attribute to HTML tag
    - Add lang="ru" attribute to improve accessibility
    
    - Fix HTML validation warning

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="h-full bg-gray-100">
+<html lang="ru" class="h-full bg-gray-100">
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
